98ee36e21a3470923bb1ace7b600c40c592824b7
Louis Chemineau <louis@chmn.me>
2023-01-15 22:22:20 +0100

    Add current location logic

--- src/components/CitySearchInput.vue
+++ src/components/CitySearchInput.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import type { Ref } from 'vue'
-import { StorageSerializers, useStorage } from '@vueuse/core'
+import { StorageSerializers, useStorage, useGeolocation } from '@vueuse/core'
+import Crosshairs from 'vue-material-design-icons/Crosshairs.vue'
+import Loading from 'vue-material-design-icons/Loading.vue'
 
 import { debounce } from 'debounce'
 import vSelect from 'vue-select'
@@ -13,6 +15,8 @@ const emits = defineEmits<{(e: 'citySelected', city: City): void}>()
 
 const selectedCity: Ref<City|null> = useStorage('selectedCity', null, undefined, { serializer: StorageSerializers.object })
 const foundCities: Ref<City[]> = ref([])
+const loadingCurrentLocation = ref(false)
+const { coords, resume, pause, isSupported } = useGeolocation({ immediate: false })
 
 if (selectedCity.value !== null) {
 	emits('citySelected', selectedCity.value)
@@ -28,109 +32,147 @@ function searchCity(query: string, toggleLoading: Function) {
 	// So vue won't update and the loading icon won't be displayed.
 	setTimeout(() => {
 		foundCities.value = searchCities(query)
-		console.log('Found cities:', foundCities.value)
+		console.debug('Found cities:', foundCities.value)
 		toggleLoading(false)
 	})
 }
 
 const debouncedSearchCity = debounce(searchCity, 200)
 
-function citySelected(city: City) {
-	console.log('City selected', city)
-	emits('citySelected', city)
+watch(selectedCity, () => {
+	if (selectedCity.value !== null) {
+		console.debug('City selected', selectedCity.value)
+		emits('citySelected', selectedCity.value)
+	}
+})
+
+watch(coords, () => {
+	console.debug('Coords changed', coords.value)
+	pause()
+	loadingCurrentLocation.value = false
+	selectedCity.value = {
+		id: -1,
+		name: 'Current location',
+		latitude: coords.value.latitude,
+		longitude: coords.value.longitude,
+		countryCode: `${coords.value.latitude.toFixed(3)}, ${coords.value.longitude.toFixed(3)}`,
+		population: 0,
+	}
+})
+
+function handleGeolocationRequest() {
+	console.debug('Requesting geolocation')
+	resume()
+	loadingCurrentLocation.value = true
 }
 </script>
 <template>
-  <v-select
-    class="city-select"
-    :options="foundCities"
-    :filterable="false"
-    v-model="selectedCity"
-    placeholder="Search a city name. Ex: Paris"
-    @search="debouncedSearchCity"
-    @option:selected="citySelected"
-    :getOptionLabel="(city: City) => `${city.name} (${city.countryCode})`"
-  >
-    <template #no-options> Type to search a city</template>
-  </v-select>
+	<div class="location-input">
+		<v-select
+			class="city-select"
+			:options="foundCities"
+			:filterable="false"
+			v-model="selectedCity"
+			placeholder="Search a city name. Ex: Paris"
+			@search="debouncedSearchCity"
+			:getOptionLabel="(city: City) => `${city.name} (${city.countryCode})`"
+		>
+			<template #no-options> Type to search a city</template>
+		</v-select>
+		<button v-if="isSupported" class="geolocation-button" @click="handleGeolocationRequest">
+			<Loading v-if="loadingCurrentLocation"/>
+			<Crosshairs v-else/>
+		</button>
+	</div>
 </template>
 <style scoped lang="scss">
-.v-select {
-  border-color: none;
-  background-color: var(--color-ternary-background);
-  border-radius: 6px;
-  width: 50%;
+.location-input {
+	display: flex;
+	width: 100%;
+	background-color: var(--color-background-ternary);
+	border-radius: 6px;
+	overflow: hidden;
 
-  @media only screen and (max-width: 1000px) {
-    width: 75%;
-  }
+	.v-select {
+		flex-grow: 1;
+		width: 0;
 
-  @media only screen and (max-width: 800px) {
-    width: 100%;
-  }
+		:deep(.vs__search) {
+			height: 60px;
+			font-size: 24px;
+			margin-top: 6px;
+			color: var(--color-text);
+		}
 
-  :deep(.vs__search) {
-    height: 60px;
-    font-size: 24px;
-    margin-top: 6px;
-    color: var(--color-text);
-  }
+		:deep(.vs__selected-options) {
+			width: calc(100% - 30px);
+		}
 
-  :deep(.vs__selected) {
-    font-size: 24px;
-    color: var(--color-primary);
-  }
+		:deep(.vs__selected) {
+			font-size: 24px;
+			color: var(--color-primary);
+			width: calc(100% - 20px);
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
 
-  &.vs--open,
-  &.vs--loading {
-    :deep(.vs__selected) {
-      top: 50%;
-      margin-top: -16px;
-    }
-  }
+		&.vs--open,
+		&.vs--loading {
+			:deep(.vs__selected) {
+				top: 50%;
+				margin-top: -16px;
+			}
+		}
 
-  :deep(.vs__actions) {
-    margin-right: 4px;
+		:deep(.vs__actions) {
+			margin-right: 4px;
 
-    .vs__clear {
-      scale: 1.5;
-      fill: var(--color-text);
-    }
+			.vs__clear {
+				scale: 1.5;
+				fill: var(--color-text);
+			}
 
-    .vs__open-indicator {
-      display: none;
-      color: var(--color-text);
-    }
+			.vs__open-indicator {
+				display: none;
+				color: var(--color-text);
+			}
 
-    .vs__spinner {
-      border-top-color: var(--color-border);
-      border-right-color: var(--color-border);
-      border-bottom-color: var(--color-border);
-      border-left-color: var(--color-text);
-    }
-  }
+			.vs__spinner {
+				border-top-color: var(--color-border);
+				border-right-color: var(--color-border);
+				border-bottom-color: var(--color-border);
+				border-left-color: var(--color-text);
+			}
+		}
 
-  &.vs--loading {
-    :deep(.vs__clear) {
-      display: none;
-    }
-  }
+		&.vs--loading {
+			:deep(.vs__clear) {
+				display: none;
+			}
+		}
 
-  :deep(.vs__dropdown-menu) {
-    background-color: var(--color-ternary-background);
-    padding: 20px 8px;
+		:deep(.vs__dropdown-menu) {
+			background-color: var(--color-background-ternary);
+			padding: 20px 8px;
 
-    .vs__dropdown-option {
-      font-size: 20px;
-      border-radius: 8px;
-      padding: 4px 8px;
-      border: 2px solid transparent;
-    }
+			.vs__dropdown-option {
+				font-size: 20px;
+				border-radius: 8px;
+				padding: 4px 8px;
+				border: 2px solid transparent;
+			}
 
-    .vs__dropdown-option--highlight {
-      border: 2px solid var(--color-primary);
-      background-color: var(--color-primary-transparent);
-    }
-  }
+			.vs__dropdown-option--highlight {
+				border: 2px solid var(--color-primary);
+				background-color: var(--color-primary-transparent);
+			}
+		}
+	}
+
+	.geolocation-button {
+		width: 72px;
+		flex-shrink: 0;
+	}
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,36 @@
 <template>
-  <header>
-    <img alt="meteo logo" src="@/assets/logo.svg" />
+	<header>
+		<img alt="meteo logo" src="@/assets/logo.svg" />
 
-    <div class="header__content">
-      <slot></slot>
-    </div>
-  </header>
+		<div class="header__content">
+			<slot></slot>
+		</div>
+	</header>
 </template>
 
 <style scoped lang="scss">
 header {
-  width: 100%;
-  height: 80px;
-  padding: 12px;
-  display: flex;
-  align-items: center;
-  background-color: var(--color-secondary-background);
-  box-sizing: border-box;
+	width: 100%;
+	height: 80px;
+	padding: 12px;
+	display: flex;
+	align-items: center;
+	background-color: var(--color-background-secondary);
+	box-sizing: border-box;
 
-  img {
-    width: 150px;
-    height: 100%;
-    margin-right: 12px;
+	img {
+		width: 150px;
+		height: 100%;
+		margin-right: 12px;
 
-    @media only screen and (max-width: 800px) {
-      width: 100px;
-    }
-  }
+		@media only screen and (max-width: 800px) {
+			width: 100px;
+		}
+	}
 
-  .header__content {
-    margin-left: 12px;
-    flex-grow: 1;
-  }
+	.header__content {
+		margin-left: 12px;
+		flex-grow: 1;
+	}
 }
 </style>
