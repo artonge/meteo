dd13daf936915cacc2b791470c1b336e1bf97753
Louis Chemineau <louis@chmn.me>
2023-02-05 15:56:58 +0100

    Improve header rendering on small screens

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,19 +18,32 @@ header {
 	background-color: var(--color-background-secondary);
 	box-sizing: border-box;
 
+	@media only screen and (max-width: 800px) {
+		padding: 4px;
+	}
+
 	img {
 		width: 150px;
 		height: 100%;
 		margin-right: 12px;
 
 		@media only screen and (max-width: 800px) {
-			width: 100px;
+			width: 30px;
+			height: 30px;
+			object-fit: cover;
+			object-position: left;
+			margin-right: 0;
+			margin-left: 8px;
 		}
 	}
 
 	.header__content {
 		margin-left: 12px;
 		flex-grow: 1;
+
+		@media only screen and (max-width: 800px) {
+			margin-left: 8px;
+		}
 	}
 }
 </style>
