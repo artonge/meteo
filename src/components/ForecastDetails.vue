41449df751ed0e10e4462c23623e1494fae6da3b
Louis Chemineau <louis@chmn.me>
2023-02-12 22:50:51 +0100

    Extract forecast details into components

---
+++ src/components/ForecastDetails.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { format, isAfter } from 'date-fns'
+
+const props = defineProps<{
+	time: string,
+}>()
+
+</script>
+<template>
+	<div class="forecast__details">
+		<div>
+			<div class="forecast__details__title">
+				{{ format(new Date(props.time), 'ccc d') }}
+			</div>
+			<div class="forecast__details__subtitle">
+				{{ format(new Date(props.time), 'p') }}
+			</div>
+		</div>
+		<div class="forecast__details__slots">
+			<div class="forecast_details_data">
+				<slot name="detail_1"></slot>
+			</div>
+			<div class="forecast_details_data">
+				<slot name="detail_2"></slot>
+			</div>
+		</div>
+	</div>
+</template>
+<style lang="scss" scoped>
+.forecast__details {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 8px 12px;
+
+	@media only screen and (max-width: 800px) {
+		padding: 4px 8px;
+	}
+
+	&__title {
+		font-size: 24px;
+
+		@media only screen and (max-width: 800px) {
+			font-size: 20px;
+		}
+	}
+
+	&__subtitle {
+		font-size: 20px;
+
+		@media only screen and (max-width: 800px) {
+			font-size: 16px;
+		}
+	}
+
+	&__slots {
+		:deep(div) {
+
+			display: flex;
+
+			& span:first-child {
+				text-align: right;
+				width: 150px;
+				margin-right: 8px;
+			}
+		}
+	}
+}
+</style>
--- src/components/ForecastViews/TemperatureForecast.vue
+++ src/components/ForecastViews/TemperatureForecast.vue
@@ -5,7 +5,8 @@ import Chart from 'chart.js/auto'
 import type { ForecastTimeStep } from '@/lib/met'
 import { getMax, getMin } from '@/lib/utils'
 import { defaultChartOptions, defaultChartPlugins } from './commonConfig'
-import { format, isAfter } from 'date-fns'
+import { isAfter } from 'date-fns'
+import ForecastDetails from '../ForecastDetails.vue'
 
 const props = defineProps<{
 	forecast: ForecastTimeStep[]
@@ -128,32 +129,16 @@ watch(() => props.forecast, () => createChart())
 </script>
 <template>
 	<div class="forecast">
-		<div class="forecast__details">
-			<div class="forecast__details__left">
-				<div class="forecast__details__title">
-					{{ format(new Date(hoveredDataPoint.time), 'ccc d') }}
-				</div>
-				<div class="forecast__details__subtitle">
-					{{ format(new Date(hoveredDataPoint.time), 'p') }}
-				</div>
-			</div>
-			<div class="forecast__details__right">
-				<div class="forecast__details__data forecast__details__data">
-					<span
-						class="forecast__details__data__title forecast__details__data__title--temperature">Temperature</span>
-					<span>{{
-						hoveredDataPoint?.data.instant.details?.air_temperature
-					}}°C</span>
-				</div>
-				<div class="forecast__details__data forecast__details__data">
-					<span
-						class="forecast__details__data__title forecast__details__data__title--precipitation">Précipitations</span>
-					<span>{{
-						formatNumber(hoveredDataPoint?.data.next_6_hours?.details.precipitation_amount)
-					}} mm/h</span>
-				</div>
-			</div>
-		</div>
+		<ForecastDetails :time="hoveredDataPoint.time">
+			<template #detail_1>
+				<span class="forecast__details__temperature">Temperature</span>
+				<span>{{ hoveredDataPoint?.data.instant.details?.air_temperature }}°C</span>
+			</template>
+			<template #detail_2>
+				<span class="forecast__details__precipitation">Précipitations</span>
+				<span>{{ formatNumber(hoveredDataPoint?.data.next_6_hours?.details.precipitation_amount) }} mm/h</span>
+			</template>
+		</ForecastDetails>
 		<div class="forecast__chart">
 			<canvas ref="canvas"></canvas>
 		</div>
@@ -166,47 +151,12 @@ watch(() => props.forecast, () => createChart())
 	flex-direction: column;
 
 	&__details {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 8px 24px 8px 12px;
-
-		@media only screen and (max-width: 800px) {
-			padding: 4px 8px;
+		&__temperature {
+			color: rgba(244, 137, 36, 0.8);
 		}
 
-		&__title {
-			font-size: 24px;
-
-			@media only screen and (max-width: 800px) {
-				font-size: 20px;
-			}
-		}
-
-		&__subtitle {
-			font-size: 20px;
-
-			@media only screen and (max-width: 800px) {
-				font-size: 16px;
-			}
-		}
-
-		&__data {
-			display: flex;
-			gap: 8px;
-
-			&__title {
-				text-align: right;
-				width: 150px;
-
-				&--temperature {
-					color: rgba(244, 137, 36, 0.8);
-				}
-
-				&--precipitation {
-					color: rgba(86, 160, 211, 0.8);
-				}
-			}
+		&__precipitation {
+			color: rgba(86, 160, 211, 0.8);
 		}
 	}
 
