2e50cca5744292f2f7447fdcf9cb2125e702d700
Louis Chemineau <louis@chmn.me>
2023-02-26 23:01:59 +0100

    Become a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
+import analyze from "rollup-plugin-analyzer";
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import analyze from "rollup-plugin-analyzer";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'meteo',
+				short_name: 'meteo',
+				description: 'Get weather forecasts for any place in the world!',
+				theme_color: '#ffcc00',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
