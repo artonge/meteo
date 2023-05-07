23836e247fe0e041bf8a60db33c0553c76264125
Louis Chemineau <louis@chmn.me>
2023-05-07 17:08:47 +0200

    Improve PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -10,24 +10,29 @@ export default defineConfig({
 		vue(),
 		VitePWA({
 			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			},
 			manifest: {
 				name: 'meteo',
 				short_name: 'meteo',
 				description: 'Get weather forecasts for any place in the world!',
 				theme_color: '#ffcc00',
+				background_color: "#22378b",
+				display: "standalone",
 				icons: [
 					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
+						"src": "/android-chrome-192x192.png",
+						"sizes": "192x192",
+						"type": "image/png"
 					},
 					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
+						"src": "/android-chrome-256x256.png",
+						"sizes": "256x256",
+						"type": "image/png"
 					}
-				]
-			}
+				],
+			},
 		})
 	],
 	resolve: {
