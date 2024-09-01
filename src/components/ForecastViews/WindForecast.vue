d7344d7b1b2924b771cd238c05815c713427d2f5
Louis Chemineau <louis@chmn.me>
2024-09-01 11:49:26 +0200

    feat: Add wind direction

--- src/components/ForecastViews/WindForecast.vue
+++ src/components/ForecastViews/WindForecast.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiArrowTopLeftThin } from '@mdi/js'
+
 import { getMax, getMin } from '@/lib/utils'
-import type { Forecast } from '@/lib/open-meteo';
+import type { Forecast } from '@/lib/open-meteo'
 import ForecastLayout from './ForecastLayout.vue'
 import { setupForecastView } from './forecastViewSetup'
 
@@ -60,6 +63,15 @@ const { hoveredDataPoint, canvas } = setupForecastView(
 			<span class="forecast__details__vitesse">Vitesse</span>
 			<span>{{ hoveredDataPoint.windSpeed }} {{ forecast.units.windSpeed }}</span>
 		</template>
+		<template #detail_2>
+			<span class="forecast__details__direction">Direction</span>
+			<span class="forecast__details__direction__arrow">
+				<!-- +45 to compensate the arrow default direction -->
+				<!-- +180 to point toward the south when direction is 0 -->
+				<svg-icon :style="`transform: rotate(${45 + 180 + hoveredDataPoint.windDirection}deg)`" type="mdi" :path="mdiArrowTopLeftThin" :size="20"></svg-icon>
+				<span class="forecast__details__direction__arrow__north">N</span>
+			</span>
+		</template>
 		<template #canvas>
 			<canvas ref="canvas"></canvas>
 		</template>
@@ -69,6 +81,33 @@ const { hoveredDataPoint, canvas } = setupForecastView(
 .forecast__details {
 	&__vitesse {
 		color: rgba(48, 195, 158);
+	}
+
+	&__direction {
+		&__arrow {
+			position: relative;
+			border: 1px solid var(--color-border);
+			border-radius: 100%;
+			width: 25px;
+			height: 25px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			&__north {
+				position: absolute;
+				top: -4px;
+				left: 50%;
+				margin-left: -4px;
+				font-size: 8px;
+				width: 8px;
+				height: 8px;
+				line-height: 8px;
+				text-align: center;
+				background-color: var(--color-background);
+				border-radius: 100%;
+			}
+		}
 	}
 }
 </style>
